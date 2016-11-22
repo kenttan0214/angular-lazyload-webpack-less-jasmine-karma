edbecd6e092e1e5d5c87c1fc4fdfc691d304670e
Tan Yong Kent <yong-kent.tan@hp.com>
2016-11-22 14:40:10 +0800

    update a better unit test case sample

--- app/modules/landing/landing-ctrl-spec.js
+++ app/modules/landing/landing-ctrl-spec.js
@@ -1,7 +1,18 @@
 'use strict';
+describe('test landing controller', function() {
+    var app = angular.module('kent.app.landing', []);
+    var landingCtrl = require('./landing-ctrl')(app);
+    var $scope = {};
 
-describe('example test', function() {
-  it('should be true', function() {
-    expect('foo').toBe('foo');
-  });
+    beforeEach(angular.mock.module(app.name));
+
+    beforeEach(inject(function($controller) {
+        var controller = $controller('LandingCtrl', {
+            $scope: $scope
+        });
+    }));
+
+    it('should be three', function() {
+        expect($scope.calculation(1, 2)).toEqual(3);
+    });
 });
